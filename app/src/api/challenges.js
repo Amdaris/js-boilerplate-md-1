e6bde18ddef0a9360c8d41104625bf5c7c6bc551
Oleg Rotar <oleg.rotar@amdaris.com>
2025-02-28 18:28:11 +0200

    Add challenges

---
+++ app/src/api/challenges.js
@@ -0,0 +1,18 @@
+export function challenge1(array) {
+    // Write the code for Challenge 1 here
+
+    return -1;
+}
+
+
+export function challenge2(array) {
+    // Write the code for Challenge 2 here
+
+    return [];
+}
+
+export function challenge3(order) {
+    // Write the code for Challenge 3 here
+
+    return order;
+}
--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,11 +1,127 @@
 const express = require("express");
 const router = express.Router();
+const challenges = require("./challenges");
 
+/**
+ * @swagger
+ * /ping:
+ *   get:
+ *     responses:
+ *       200:
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *           application/json:
+ *             schema:
+ *               type: string
+ *           text/json:
+ *             schema:
+ *               type: string
+ */
 router.get("/ping", (req, res) => {
   res.send({
     result: "pong"
   })
 })
 
+/**
+ * @swagger
+ * /challenge-1:
+ *   post:
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items: 
+ *               type: integer
+ *     responses:
+ *       200:
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: integer
+ *           application/json:
+ *             schema:
+ *               type: integer
+ *           text/json:
+ *             schema:
+ *               type: integer
+ */
+router.post("/challenge-1", (req, res) => {
+  const result = challenges.challenge1(req.body);
+
+  res.json(result);
+})
+
+
+/**
+ * @swagger
+ * /challenge-2:
+ *   post:
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items: 
+ *               type: integer
+ *     responses:
+ *       200:
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: array
+ *               items: 
+ *                 type: integer
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items: 
+ *                 type: integer
+ *           text/json:
+ *             schema:
+ *               type: array
+ *               items: 
+ *                 type: integer
+ */
+router.post("/challenge-2", (req, res) => {
+  const result = challenges.challenge2(req.body);
+
+  res.json(result);
+})
+
+
+/**
+ * @swagger
+ * /challenge-3:
+ *   post:
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                order:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *           application/json:
+ *             schema:
+ *               type: arrstringay
+ *           text/json:
+ *             schema:
+ *               type: string
+ */
+router.post("/challenge-3", (req, res) => {
+  const result = challenges.challenge3(req.body.order);
+
+  res.json(result);
+})
 
 module.exports = router;
