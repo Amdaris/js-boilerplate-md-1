d42ff1733891de6f5bad0ae6b62369d6b9734068
Oleg Rotar <oleg.rotar@amdaris.com>
2025-03-02 20:03:17 +0200

    Fix export

--- app/src/api/challenges.js
+++ app/src/api/challenges.js
@@ -1,18 +1,20 @@
-export function challenge1(array) {
+function challenge1(array) {
     // Write the code for Challenge 1 here
 
     return -1;
 }
 
 
-export function challenge2(array) {
+function challenge2(array) {
     // Write the code for Challenge 2 here
 
     return [];
 }
 
-export function challenge3(order) {
+function challenge3(order) {
     // Write the code for Challenge 3 here
 
     return order;
 }
+
+module.exports = { challenge1, challenge2, challenge3 };
